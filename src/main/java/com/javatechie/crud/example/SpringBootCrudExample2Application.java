a680b2bd48e696a2fca51b1305be7d50cb5a36aa
Yash Bavishi <bavishiys63@gmail.com>
2023-10-29 18:38:46 -0700

    spring-crud

--- src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -2,8 +2,14 @@ package com.javatechie.crud.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+//import javax.websocket.ClientEndpoint;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class SpringBootCrudExample2Application {
 
 	public static void main(String[] args) {
